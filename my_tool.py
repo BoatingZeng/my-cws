d58079fe09a9c3c63d5b5dc364df817c23de8646
boatingzeng <945467442@qq.com>
2018-01-23 13:55:03 +0800

    生成字典时，空格作为一个生僻字首先添加到字典里

--- my_tool.py
+++ my_tool.py
@@ -1,8 +1,9 @@
 import codecs
 
 # 提取字典，并且统计字频，在当前目录生成chars.txt，filelist是文件path数组
+# TODO 字典里不保留英文字母，特殊字符，这个要定个列表来做过滤
 def get_chars(filelist):
-    char_set = {}
+    char_set = {' ': 1}  # (普通)空格作为字典的第一个字，并且计数设定为1，把它当作生僻字来处理
     out_char = codecs.open('./chars.txt', 'w', encoding='utf-8')
     for i, file_name in enumerate(filelist):
         for line in codecs.open(file_name, 'rb', encoding='utf-8'):
